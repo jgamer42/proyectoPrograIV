a3131a6898926226ce5a51fccaaece145e064e8e
Jaime <jgaimer42@gmail.com>
2020-05-25 01:24:23 -0500

    uml terminado, ya los botones estan conectados

--- src/vista/menu_insertar.py
+++ src/vista/menu_insertar.py
@@ -2,14 +2,13 @@ from vista_base import Vista_base
 from tkinter import *
 import sys
 sys.path.append("/home/crinto/proyectos/proyecto_dani/src/controlador/")
-import controlador_insertar
+import controlador_insertar as controlador
 class Menu_insertar(Vista_base):
     def __init__(self,direccion_archivo):
         super().__init__()
         Label(self.frame,text="en donde quiere insertar").grid(row=0,column=0,columnspan = 2)
         Label(self.frame,text=direccion_archivo).grid(row=1,column=0,columnspan=2)
-        Button(self.frame,text="consumo agua persona juridica").grid(row=2,column=0)
-        Button(self.frame,text="consumo energia persona juridica").grid(row=3,column=0)
-        Button(self.frame,text="consumo agua persona natural").grid(row=2,column=1)
-        Button(self.frame,text="consumo energia persona natural").grid(row=3,column=1)
-        #TODO conectar esto al controlador
+        Button(self.frame,text="consumo agua persona juridica",command = lambda:(controlador.leer_archivo(direccion_archivo))).grid(row=2,column=0)
+        Button(self.frame,text="consumo energia persona juridica",command = lambda:(controlador.leer_archivo(direccion_archivo))).grid(row=3,column=0)
+        Button(self.frame,text="consumo agua persona natural",command = lambda:(controlador.leer_archivo(direccion_archivo))).grid(row=2,column=1)
+        Button(self.frame,text="consumo energia persona natural",command = lambda:(controlador.leer_archivo(direccion_archivo))).grid(row=3,column=1)
